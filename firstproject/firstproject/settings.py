fdcda1f13a3ba6f6d035044a78f9f4d5fec1eddb
rochan <rochan1999@gmail.com>
2026-03-15 14:31:04 +0530

    mod dbsqlite to mysql change

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "firstproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django.db",
+        "USER": "root",
+        "PASSWORD": "sql1234",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"init_commands": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
