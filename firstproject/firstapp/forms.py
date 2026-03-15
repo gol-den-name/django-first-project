15e3161ba1b4a3dacfdc3b69d33022cc2eaa92b8
rochan <rochan1999@gmail.com>
2026-03-15 15:46:14 +0530

    fixed issue with forms after runserver, added new path for reservation, created templates folder-indexhtml, frms urls views migrated

--- firstproject/firstapp/forms.py
+++ firstproject/firstapp/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-
 from .models import Reservation
 
 
-class ReservationForm(forms.modelForm):
+class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = "__all__"
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 
 # Create your views here.
@@ -11,3 +12,14 @@ def hello_world(request):
 class HelloEthiopia(View):
     def get(self, request):
         return HttpResponse("hello Ethiopia")
+
+
+def home(request):
+    form = ReservationForm()
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("success")
+
+    return render(request, "index.html", {"form": form})
